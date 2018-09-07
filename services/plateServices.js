b61f4b53cc198f279b8bfa51354ea1f2e9c0e692
tasiya <siyandamakhalima.1@gmail.com>
2018-09-07 14:27:06 +0200

    added the first test. Count the rows of the table registration

--- services/plateServices.js
+++ services/plateServices.js
@@ -22,7 +22,7 @@ module.exports = function (pool) {
     }
     async function countAll () {
         let count = await pool.query('select count(*) FROM registration_numbers');
-        return count.rows[0].count;
+        return parseInt(count.rows[0].count);
     }
     async function insertPlate (plate, locationID) {
         await pool.query('insert into registration_numbers (plates,towns_id,status) values ($1,$2,$3)',[plate,locationID,'Not found']);
--- test/registration_numbers_webapp.test.js
+++ test/registration_numbers_webapp.test.js
@@ -1,3 +1,45 @@
 const services = require('../services/plateServices');
 const assert = require('assert');
 const pg = require('pg');
+const Pool = pg.Pool;
+
+let useSSL = false;
+let local = process.env.LOCAL || false;
+
+if (process.env.DATABASE_URL && !local) {
+    useSSL = true;
+}
+
+const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/registration_app_database';
+
+const pool = new Pool({
+    connectionString,
+    ssl: useSSL
+});
+
+describe('Registratiob number Web Application test(s)', function () {
+    describe('Counting data in database', function () {
+        beforeEach(async function () {
+            await pool.query('delete from registration_numbers');
+        });
+        it('it should return zero(0) if not data', async function () {
+            let Service = services(pool);
+            let result = await Service.countAll();
+            assert.strictEqual(result, 0); 
+        });
+        it('it should return 4 if not data', async function () {
+            let Service = services(pool);
+            await Service.tryAddPlate('CA 123-321',1);
+            await Service.tryAddPlate('CA 321-123',1);
+            await Service.tryAddPlate('CY 321-123',1);
+            await Service.tryAddPlate('CL 321-123',1);
+            let result = await Service.countAll();
+            assert.strictEqual(result, 4); 
+        });
+    });
+    
+
+    after(function () {
+        pool.end();
+    });
+});
